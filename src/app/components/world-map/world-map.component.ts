83818d5405167b5aad64e05cc29570d526843b60
cornrowsMatt <mtuson1414@gmail.com>
2023-11-11 17:35:48 +1300

    added type safety, and cleaned up file

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -2,7 +2,7 @@ import * as mapboxgl from 'mapbox-gl';
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environments';
-import { LngLatLike } from 'mapbox-gl';
+import { LngLatLike, MapMouseEvent } from 'mapbox-gl';
 
 @Component({
   selector: 'app-world-map',
@@ -54,23 +54,76 @@ export class WorldMapComponent implements OnInit {
     map.addControl(new mapboxgl.NavigationControl());
     map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
 
-    const marker2 = new mapboxgl.Marker({ color: 'red', anchor: 'center' })
+    const marker2 = new mapboxgl.Marker({ color: 'red', anchor: 'bottom' })
       .setLngLat(coordinates)
       .addTo(map);
 
     const popup = new mapboxgl.Popup({ closeOnClick: false })
       .setLngLat(this.aucklandCoordinates)
       .setHTML('<h3>Auckland!</h3>')
-      .addTo(map)
+      .addTo(map);
 
-    map.on('click', (e: any) => {
-      const clickedCoordinates = e.lngLat.toArray();
-
+    map.on('click', (event: MapMouseEvent) => {
+      const clickedCoordinates: LngLatLike = event.lngLat;
       const marker = new mapboxgl.Marker()
         .setLngLat(clickedCoordinates)
         .addTo(map);
       this.markers.push(marker);
     });
+
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        localGeocoder: (query: string) => this.coordinatesGeocoder(query),
+        zoom: 10,
+        placeholder: 'Search By Coords (Long, Lat)',
+        mapboxgl: mapboxgl,
+        reverseGeocode: true,
+      })
+    );
+  }
+
+  coordinatesGeocoder(query: string): MapboxGeocoder.Result[] {
+    const matches = query.match(
+      /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+    );
+
+    if (!matches) {
+      return [];
+    }
+
+    function coordinateFeature(lng: number, lat: number): any {
+      return {
+        center: [lng, lat],
+        geometry: {
+          type: 'Point',
+          coordinates: [lng, lat],
+        },
+        place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+        place_type: ['coordinate'],
+        properties: {},
+        type: 'Feature',
+      };
+    }
+
+    const coord1: number = Number(matches[1]);
+    const coord2: number = Number(matches[2]);
+    const geocodes: any[] = [];
+
+    if (coord1 < -90 || coord1 > 90) {
+      geocodes.push(coordinateFeature(coord1, coord2));
+    }
+
+    if (coord2 < -90 || coord2 > 90) {
+      geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    if (geocodes.length === 0) {
+      geocodes.push(coordinateFeature(coord1, coord2));
+      geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    return geocodes;
   }
 
   removeAllMarkers() {
@@ -81,4 +134,3 @@ export class WorldMapComponent implements OnInit {
 function addClassName(arg0: string) {
   throw new Error('Function not implemented.');
 }
-
