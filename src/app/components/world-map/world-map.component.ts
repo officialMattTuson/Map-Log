8c90389757015c9c220711a6e3a67321693ce493
cornrowsMatt <mtuson1414@gmail.com>
2023-11-28 23:58:54 +1300

    refactoring world map component, reducing size of some function, split popup functionalities into own methods

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -4,6 +4,7 @@ import {
   AfterViewInit,
   Component,
   ComponentFactoryResolver,
+  ComponentRef,
   ElementRef,
   OnInit,
   Renderer2,
@@ -31,6 +32,7 @@ export class WorldMapComponent implements OnInit {
   public aucklandCoordinates: LngLatLike = [174.7645, -36.8509];
   public selectedLocation: string;
   public isExistingMarker = false;
+  public map: mapboxgl.Map;
 
   isOverlayOpen$ = this.mapOverlayService.isOverlayOpen$;
 
@@ -58,10 +60,7 @@ export class WorldMapComponent implements OnInit {
   }
 
   onLocationSuccess(position: GeolocationPosition) {
-    const centralCoords: LngLatLike = [
-      position.coords.longitude,
-      position.coords.latitude,
-    ];
+    const centralCoords: LngLatLike = [position.coords.longitude, position.coords.latitude];
     this.setupMap(centralCoords);
   }
 
@@ -71,93 +70,90 @@ export class WorldMapComponent implements OnInit {
 
   setupMap(coordinates: LngLatLike) {
     (mapboxgl as typeof mapboxgl).accessToken = environment.mapboxAccessToken;
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/satellite-streets-v11',
       center: coordinates,
       zoom: 8,
     });
 
-    this.searchGeocoder(map);
-    this.setUpMapControls(map);
-    this.createMarker(map);
+    this.searchGeocoder();
+    this.setUpMapControls();
+    this.handleMarkerAndPopupCreation();
   }
 
-  setUpMapControls(map: mapboxgl.Map) {
+  setUpMapControls() {
     const scale = new mapboxgl.ScaleControl({
       maxWidth: 200,
       unit: 'metric',
     });
-    map.addControl(scale, 'bottom-right');
-    map.addControl(new mapboxgl.NavigationControl());
-    map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
+    this.map.addControl(scale, 'bottom-right');
+    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
   }
 
-  createMarker(map: mapboxgl.Map) {
-    map.on('click', (event: MapMouseEvent) => {
+  handleMarkerAndPopupCreation() {
+    this.map.on('click', (event: MapMouseEvent) => {
       const coordinates = event.lngLat.toArray();
 
       this.getLocationInfoAtCoordinates(coordinates).subscribe({
         next: (locationInfo: string) => {
           this.selectedLocation = locationInfo;
-          const clickedMarker = this.findMarkerByCoordinates(map, event.lngLat);
-          const popupFactory =
-            this.factoryResolver.resolveComponentFactory(MarkerPopupComponent);
+          const clickedMarker = this.findMarkerByCoordinates(event.lngLat);
+          const popupFactory = this.factoryResolver.resolveComponentFactory(MarkerPopupComponent);
 
-          const popupComponentRef =
-            this.popupHost.viewContainerRef.createComponent(popupFactory);
+          const popupComponentRef = this.popupHost.viewContainerRef.createComponent(popupFactory);
           popupComponentRef.instance.location = this.selectedLocation;
-          let popup: mapboxgl.Popup;
-          if (clickedMarker) {
-            this.isExistingMarker = true;
-            popupComponentRef.instance.selectedStoryMarker = clickedMarker;
-            popupComponentRef.instance.storyMarkers = this.storyMarkers;
-            const markerScreenPoint = map.project(clickedMarker.marker.getLngLat());
-            const popupLngLat = map.unproject(
-              markerScreenPoint.add(new mapboxgl.Point(0, -40)),
-            );
-            popup = new mapboxgl.Popup()
-              .setLngLat(popupLngLat)
-              .setDOMContent(popupComponentRef.location.nativeElement)
-              .addTo(map);
-            this.overridePopupStyles(popup);
-            popupComponentRef.instance.popup = popup;
-          } else {
-            this.isExistingMarker = false;
-            popup = new mapboxgl.Popup({closeOnClick: true})
-              .setLngLat(event.lngLat)
-              .setDOMContent(popupComponentRef.location.nativeElement)
-              .addTo(map);
-
-            popupComponentRef.instance.placeMarkerConfirmationPopup = true;
-            popupComponentRef.instance.placeMarkerClicked.subscribe({
-              next: () => this.handleMarkerPlacedEvent(popup, event, map),
-            });
-          }
+          clickedMarker
+            ? this.onExistingPopupClick(popupComponentRef, clickedMarker)
+            : this.addMarkerToMap(popupComponentRef, event);
         },
       });
     });
   }
 
-  findMarkerByCoordinates(
-    map: mapboxgl.Map,
-    lngLat: mapboxgl.LngLat,
-  ): StoryMarker | undefined {
-    return this.storyMarkers.find(storyMarker => {
-      const markerScreenPoint = map.project(storyMarker.marker.getLngLat());
-      return markerScreenPoint.dist(map.project(lngLat)) < 45;
+  onExistingPopupClick(
+    popupComponentRef: ComponentRef<MarkerPopupComponent>,
+    clickedMarker: StoryMarker,
+  ) {
+    let popup: mapboxgl.Popup;
+    this.isExistingMarker = true;
+    popupComponentRef.instance.selectedStoryMarker = clickedMarker;
+    popupComponentRef.instance.storyMarkers = this.storyMarkers;
+    const markerScreenPoint = this.map.project(clickedMarker.marker.getLngLat());
+    const popupLngLat = this.map.unproject(markerScreenPoint.add(new mapboxgl.Point(0, -40)));
+    popup = new mapboxgl.Popup()
+      .setLngLat(popupLngLat)
+      .setDOMContent(popupComponentRef.location.nativeElement)
+      .addTo(this.map);
+    this.overridePopupStyles(popup);
+    popupComponentRef.instance.popup = popup;
+  }
+
+  addMarkerToMap(popupComponentRef: ComponentRef<MarkerPopupComponent>, event: MapMouseEvent) {
+    let popup: mapboxgl.Popup;
+    this.isExistingMarker = false;
+    popup = new mapboxgl.Popup({closeOnClick: true})
+      .setLngLat(event.lngLat)
+      .setDOMContent(popupComponentRef.location.nativeElement)
+      .addTo(this.map);
+
+    popupComponentRef.instance.placeMarkerConfirmationPopup = true;
+    popupComponentRef.instance.placeMarkerClicked.subscribe({
+      next: () => this.handleMarkerPlacedEvent(popup, event),
     });
   }
 
-  handleMarkerPlacedEvent(
-    popup: mapboxgl.Popup,
-    event: mapboxgl.MapMouseEvent,
-    map: mapboxgl.Map,
-  ) {
+  findMarkerByCoordinates(lngLat: mapboxgl.LngLat): StoryMarker | undefined {
+    return this.storyMarkers.find(storyMarker => {
+      const markerScreenPoint = this.map.project(storyMarker.marker.getLngLat());
+      return markerScreenPoint.dist(this.map.project(lngLat)) < 45;
+    });
+  }
+
+  handleMarkerPlacedEvent(popup: mapboxgl.Popup, event: mapboxgl.MapMouseEvent) {
     popup.getElement().addEventListener('click', () => {
-      const marker = new mapboxgl.Marker()
-        .setLngLat(event.lngLat)
-        .addTo(map);
+      const marker = new mapboxgl.Marker().setLngLat(event.lngLat).addTo(this.map);
       popup.remove();
       const newStoryMarker: StoryMarker = {
         marker: marker,
@@ -178,8 +174,8 @@ export class WorldMapComponent implements OnInit {
     );
   }
 
-  searchGeocoder(map: mapboxgl.Map) {
-    map.addControl(
+  searchGeocoder() {
+    this.map.addControl(
       new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
         localGeocoder: (query: string) => this.coordinatesGeocoder(query),
@@ -192,9 +188,7 @@ export class WorldMapComponent implements OnInit {
   }
 
   coordinatesGeocoder(query: string): MapboxGeocoder.Result[] {
-    const matches = query.match(
-      /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i,
-    );
+    const matches = query.match(/^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i);
 
     if (!matches) {
       return [];
@@ -248,9 +242,7 @@ export class WorldMapComponent implements OnInit {
     popupElement.style.height = '15rem';
     const closeButtonElement = popup
       .getElement()
-      .parentElement?.getElementsByClassName(
-        'mapboxgl-popup-close-button',
-      )[0] as HTMLElement;
+      .parentElement?.getElementsByClassName('mapboxgl-popup-close-button')[0] as HTMLElement;
     closeButtonElement.style.top = '-114%';
     closeButtonElement.style.left = '100%';
   }
